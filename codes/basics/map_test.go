863b2abdb1e26b61756aa5c6d4c4d80e088fb914
Po <chenjunfeng@ethstorage.io>
2025-05-28 16:22:55 +0800

    add map initialization

--- codes/basics/map_test.go
+++ codes/basics/map_test.go
@@ -1,6 +1,9 @@
 package basics
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 type Value struct {
 	val int
@@ -31,4 +34,13 @@ func TestStructWithMapInitalization(t *testing.T) {
 	s := S{map[int]int{}}
 	s.M[0] = 1
 	println(s.M[0])
+}
+
+func TestNestedMap(t *testing.T) {
+	// 双层map可以自动初始化
+	m := map[int]map[int]int{}
+	a := m[0]
+	fmt.Println("layer1 value:", a)
+	b := a[0]
+	fmt.Println("layer2 value:", b)
 }
