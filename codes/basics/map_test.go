612bb93b4973d55931b022243b5b3b75e7fda0b3
Po <chenjunfeng@ethstorage.io>
2025-05-28 13:04:00 +0800

    add map test

---
+++ codes/basics/map_test.go
@@ -0,0 +1,22 @@
+package basics
+
+import "testing"
+
+type Value struct {
+	val int
+}
+
+func TestMapOveride(t *testing.T) {
+	m := map[int]Value{}
+	v := m[0]
+	println(v.val)
+	v.val = 1
+	// cannot override map value directly
+	println(v.val, m[0].val)
+
+	mp := map[int]*Value{}
+	mp[0] = &Value{val: 0}
+	vmp := mp[0]
+	vmp.val = 1
+	println(vmp.val, mp[0].val)
+}
