b23bb77a88e83539b81d1dba6120c6122ae84ff4
Po <wwwcjf163@163.com>
2025-08-26 01:00:25 +0800

    add defer test

--- codes/basics/nested_defer_test.go
+++ codes/basics/nested_defer_test.go
@@ -18,3 +18,19 @@ func TestNestedDefer(t *testing.T) {
 	}
 	fmt.Println("main ends")
 }
+
+type deferStruct struct {
+	ret int
+}
+
+// ret will be set before return
+func (a *deferStruct) deferReturn() (ret int) {
+	defer func() { a.ret = ret }()
+	return 1
+}
+
+func TestDeferReturn(t *testing.T) {
+	s := deferStruct{}
+	s.deferReturn()
+	println(s.ret)
+}
