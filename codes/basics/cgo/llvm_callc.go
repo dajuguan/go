c23fb634f25e7df2e8f3ceff7d21fad7e0839a22
Po <wwwcjf163@163.com>
2025-09-30 14:48:02 +0800

    add example for call c function which depends on libc

--- codes/basics/cgo/llvm_callc.go
+++ codes/basics/cgo/llvm_callc.go
@@ -8,7 +8,6 @@ package main
 import "C"
 import (
 	"fmt"
-	"unsafe"
 
 	llvm "tinygo.org/x/go-llvm"
 )
@@ -21,10 +20,15 @@ func LLVMCallC() {
 	mod := ctx.NewModule("mymodule")
 	builder := ctx.NewBuilder()
 
-	// Declare external hello_from_c : void(i32)
+	// Declare external squre
 	int32Type := llvm.GlobalContext().Int32Type()
 	fnType := llvm.FunctionType(int32Type, []llvm.Type{int32Type}, false)
-	fn := llvm.AddFunction(mod, "square_from_c", fnType)
+	// shoule use the same name, if we don't link it in runtime with AddGlobalMapping
+	fn := llvm.AddFunction(mod, "square", fnType)
+
+	// Declare cmalloc
+	fnMallocType := llvm.FunctionType(int32Type, []llvm.Type{}, false)
+	fnMalloc := llvm.AddFunction(mod, "cmalloc", fnMallocType)
 
 	// main function
 	mainFnType := llvm.FunctionType(int32Type, nil, false)
@@ -32,16 +36,22 @@ func LLVMCallC() {
 	entry := ctx.AddBasicBlock(mainFn, "entry")
 	builder.SetInsertPointAtEnd(entry)
 
-	x := llvm.ConstInt(int32Type, 2, false)
+	x := llvm.ConstInt(int32Type, 1, false)
 	ret := builder.CreateCall(fnType, fn, []llvm.Value{x}, "")
+	fmt.Println("ret:", ret)
+	ret = builder.CreateCall(fnMallocType, fnMalloc, []llvm.Value{}, "")
 	builder.CreateRet(ret)
 
 	// Execution engine
 	engine, _ := llvm.NewExecutionEngine(mod)
 
 	// Load symbol from shared lib (no cgo "import C" trickery, just dlopen/dlsym)
-	sym := C.load_symbol(C.CString("./libmylib.so"), C.CString("square"))
-	engine.AddGlobalMapping(fn, unsafe.Pointer(sym))
+	// sym := C.load_symbol(C.CString("./libmylib.so"), C.CString("square"))
+	// engine.AddGlobalMapping(fn, unsafe.Pointer(sym))
+
+	// Only object file or dynamic linked object is needed in llvm.
+	// Don't use archive file, cause it can automatically link libc, we don't need to worry about it.
+	engine.AddObjectFileByFilename("./mylib.o")
 
 	// Run main
 	res := engine.RunFunction(mainFn, nil)
--- codes/basics/cgo/mylib.c
+++ codes/basics/cgo/mylib.c
@@ -1,4 +1,29 @@
+/*
+gcc -c mylib.c -o mylib.o
+ar rcs mylib.a mylib.o 
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
 int square(int x) {
     return x + x;
+}
+
+int cmalloc() {
+    // Allocate 128 bytes
+    void* ptr = malloc(128);
+    if (ptr == NULL) {
+        printf("malloc failed!\n");
+        return 5;
+    }
+    printf("memory_new allocated at %p\n", ptr);
+
+    // Optionally: initialize memory
+    for (int i = 0; i < 128; i++) {
+        ((char*)ptr)[i] = 0;
+    }
+
+    // Free the memory
+    free(ptr);
+    return 6;
 }
